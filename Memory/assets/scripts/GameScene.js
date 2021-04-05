6e696a3297dea228c5166c11e073904470025696
Doomer77 <newzibkoff1984@gmail.com>
2021-04-05 12:03:53 +0300

    add card class

---
+++ Memory/assets/scripts/GameScene.js
@@ -0,0 +1,48 @@
+class GameScene extends Phaser.Scene {
+    constructor () {
+        super ("Game");
+    }
+    preload () {
+        // 1. загрузить бэкгранд
+        // console.log(this === true); true
+        //Метод для загрузки изображений
+        this.load.image("bg", "assets/sprites/background.png");
+        this.load.image("card", "assets/sprites/card.png");
+    }
+    create () {
+        // 2. вывести бэкграунд
+        //Метод загрузки спрайтов объекта
+        // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
+        this.createBackground();
+        
+        //Получение координат карт
+        this.createCard();
+    }
+    createBackground () {
+      this.add.sprite(0, 0, "bg").setOrigin(0, 0);
+    }
+    createCard () {
+      this.cards = [];
+      let positions = this.getCardsPositions();
+      for (let position of positions) {
+        this.cards.push(new Card(this, position));
+      }
+    }
+    getCardsPositions () {
+        let positions = [];
+        let cardTexture = this.textures.get("card").getSourceImage();
+        let cardWidth = cardTexture.width + 4;
+        let cardHeight = cardTexture.height + 4;
+        let offsetX = (this.sys.game.config.width - cardWidth * config.cols) / 2;
+        let offsetY = (this.sys.game.config.height - cardHeight * config.rows) / 2;
+        for (let row = 0; row < config.rows; row++) {
+          for (let col = 0; col < config.cols; col++) {
+            positions.push({
+              x: offsetX + col * cardWidth,
+              y: offsetY + row * cardHeight,
+            });
+          }
+        }
+        return positions;
+    }
+};
