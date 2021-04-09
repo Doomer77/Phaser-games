c8406acb576ab1740345e975e19533bee0f90bdb
Doomer77 <newzibkoff1984@gmail.com>
2021-04-09 18:05:18 +0300

    add interactive card

--- Memory/assets/scripts/GameScene.js
+++ Memory/assets/scripts/GameScene.js
@@ -2,6 +2,7 @@ class GameScene extends Phaser.Scene {
     constructor () {
         super ("Game");
     }
+
     preload () {
         // 1. загрузить бэкгранд
         // console.log(this === true); true
@@ -15,18 +16,19 @@ class GameScene extends Phaser.Scene {
         this.load.image("card4", "assets/sprites/card4.png");
         this.load.image("card5", "assets/sprites/card5.png");
     }
+
     create () {
         // 2. вывести бэкграунд
-        //Метод загрузки спрайтов объекта
-        // this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'bg');
         this.createBackground();
         
         //Получение координат карт
         this.createCard();
     }
+
     createBackground () {
       this.add.sprite(0, 0, "bg").setOrigin(0, 0);
     }
+
     createCard () {
       this.cards = [];
       let positions = this.getCardsPositions();
@@ -36,7 +38,13 @@ class GameScene extends Phaser.Scene {
           this.cards.push(new Card(this, value, positions.pop()));
         }
       }
+      this.input.on('gameobjectdown', this.onCardClicked, this);
     }
+
+    onCardClicked (pointer, card) {
+      card.open();
+    }
+
     getCardsPositions () {
         let positions = [];
         let cardTexture = this.textures.get("card").getSourceImage();
