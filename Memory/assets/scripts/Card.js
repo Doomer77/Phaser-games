07e124b3fb6f2782a8691fa45e85b30e594141a3
Doomer77 <newzibkoff1984@gmail.com>
2021-04-21 19:41:38 +0300

    add tweens animations cards

--- Memory/assets/scripts/Card.js
+++ Memory/assets/scripts/Card.js
@@ -3,15 +3,36 @@ class Card extends Phaser.GameObjects.Sprite {
         super (scene, 0, 0, 'card');
         this.scene = scene;
         this.value = value;
-        this.setOrigin(0, 0);
         this.scene.add.existing(this);
         this.setInteractive();
         this.opened = false;
-        // this.on('pointerdown', this.open, this);
     }
+
+        flip () {
+            this.scene.tweens.add ({
+                targets: this,
+                scaleX: 0,
+                ease: 'Linear',
+                duration: 150,
+                onComplete: () => {
+                   this.show();
+                }
+            });
+        }
+
+        show () {
+            this.setTexture('card' + this.value);
+            this.scene.tweens.add({
+                targets: this,
+                scaleX: 1,
+                ease: 'Linear',
+                duration: 150,
+            });
+        }
+
         open () {
             this.opened = true;
-            this.setTexture('card' + this.value);
+            this.flip();
         }
 
         close () {
